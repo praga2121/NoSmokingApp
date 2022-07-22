7abde13d15693e5520d68bf688b68f7562ccfcd6
Theshan08 <theshanraj13@gmail.com>
2022-07-22 13:33:12 +0800

    Initial commit

---
+++ app/src/main/java/com/example/googleads/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.googleads;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AdView mAdView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, initializationStatus -> {
+
+        });
+
+
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
+
