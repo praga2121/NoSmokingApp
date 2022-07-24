613aa534270556170b8d73cc9b583323fd3da014
[Tan Zhi Shan] <[heliosjun@ymail.com]>
2022-07-25 03:32:43 +0800

    Project merging, currently merged - ads

--- app/src/main/java/com/example/nosmoking/MainActivity.java
+++ app/src/main/java/com/example/nosmoking/MainActivity.java
@@ -9,17 +9,29 @@ import android.view.View;
 
 
 import com.example.nosmokingdb.NoSmokingDatabase;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
 
 public class MainActivity extends AppCompatActivity {
     NoSmokingDatabase noSmokingDatabase;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        MobileAds.initialize(this, initializationStatus -> {
+
+        });
+
         noSmokingDatabase = Room.databaseBuilder(getApplicationContext(), NoSmokingDatabase.class, "new_smoking_tips").allowMainThreadQueries().build();
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     public void smokingTips_onClick(View view) {
